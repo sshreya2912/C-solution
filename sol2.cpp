3d05b35de93285c2dfefd755af52f2b4b494745c
Shreya <52084473+sshreya2912@users.noreply.github.com>
2020-11-17 19:54:39 +0530

    Create sol2.cpp

---
+++ sol2.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include<map>
+using namespace std;
+int main()
+{
+	string s1 = "XYYZXZYZXXYZ";
+	string s2 = "XYZ";
+
+	if(s2.length()>s1.length())
+    {
+        cout<<"-1"<<endl;
+    
+    }
+    else
+    {
+        for(int i=0;i<=s1.length()-s2.length();i++)
+        {
+            if (is_permutation(s1.begin() + i, s1.begin() + i + s2.length(), s2.begin()))
+		{
+			cout <<  s1.substr(i,s2.length()) <<" at index "<<i<<endl;
+					
+		}
+
+        }
+    }
+    
+
+	return 0;
+}
