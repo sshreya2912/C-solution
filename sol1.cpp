8aa440fee2df097c4199b93f62f028a1e079e8e6
Shreya <52084473+sshreya2912@users.noreply.github.com>
2020-11-17 19:34:28 +0530

    Create sol1.cpp

---
+++ sol1.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<algorithm>
+#include<cmath>
+#define ll long long
+using namespace std;
+int main()
+{
+    //assuming the pattern has to be printed in continuation as given in the question
+   int n=7;
+   int x;
+    for(ll i=1;i<=2*n-1;i++)
+    {
+       
+      x=(i>n?(n-(i%n)):i);
+     
+        for(ll j=x;j<=n;j++)
+        {
+            cout<<j<<" ";
+        }
+        
+       
+    }
+
+}
